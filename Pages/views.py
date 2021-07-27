b63b7441614deafe723cde91579faaf45b164e6c
Parkjju <rudwns3927@gmail.com>
2021-07-27 15:44:39 +0900

    Add: django pages with base.html

---
+++ Pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def noticeBoard(request):
+    return render(request,'notice/notice-board.html')
+
+def noticePost(request):
+    return render(request, 'notice/notice-post.html')
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def purchase(request):
+    return render(request, 'goods/purchase.html')
+
+def register(request):
+    return render(request, 'goods/goodsRegister.html')
