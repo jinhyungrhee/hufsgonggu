1113b0f89765d895c379c5eaa88f4e12175eace3
Jinhyung Rhee <rheejinhyung@gmail.com>
2021-08-12 02:09:14 +0900

    Add : 후기 작성 구현(review-post.html)

--- APIs/models.py
+++ APIs/models.py
@@ -16,3 +16,22 @@ class Product(models.Model):
         db_table = 'products_product'
         verbose_name = '상품'
         verbose_name_plural = '상품'
+
+# Review 테이블 정의
+class Review(models.Model):
+    title = models.CharField(max_length=256, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    image = models.ImageField(upload_to="review/", blank=True, null=True)
+    category = models.CharField(max_length=20, verbose_name='카테고리')
+    store = models.CharField(max_length=20, verbose_name='업체')
+    delivery = models.IntegerField(verbose_name='배송기간')
+    price = models.IntegerField(verbose_name='가격')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'review'
+        verbose_name = '리뷰'
+        verbose_name_plural = '리뷰'
--- APIs/views.py
+++ APIs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView, CreateView, ListView
 from django.views.generic.list import MultipleObjectMixin
-from .models import Product
+from .models import Product, Review
 from .forms import RegisterForm
 
 class ProductList(ListView):
@@ -14,6 +14,20 @@ class ProductCreate(CreateView):
     fields = '__all__'
     template_name = 'goods/goodsRegister.html'
     success_url = '../complete/'
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        return super().post(request, *args, **kwargs)
+
+class ReviewList(ListView):
+    model = Review
+    template_name = 'review/reviewList.html'
+
+class ReviewCreate(CreateView):
+    model = Review
+    fields = '__all__'
+    template_name = 'review/review-post.html'
+    success_url = '../complete2/'
 
     def post(self, request, *args, **kwargs):
         self.object = None
